53c00626f8340fc5dd11292e7cd53a8fd4c2d6a2
AneelGunji <gunji.aneel@gmail.com>
2019-10-31 06:57:25 +0530

    basic update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Beinghealthy
+# Ganga
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.9.
 
 ## Development server
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to beinghealthy!');
+    expect(page.getTitleText()).toEqual('ganga app is running!');
   });
 
   afterEach(async () => {
